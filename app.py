a0ff5cf11f0514a21f9dc205dc4898cae47586a7
AzureAD\SameerUmadi <sameer.umadi143@gmail.com>
2025-06-20 09:56:00 +0400

    fix: correct capitalization in greeting message in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, techcarrotian's welcome to our DevSecOps pipeline demo!"
+    return "Hello, techcarrotian's Welcome to our DevSecOps pipeline demo!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
